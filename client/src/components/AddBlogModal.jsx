11e96960719f395a8fe25c99000ca85f64caba19
RAGHUKHAJURIA <khajuriaraghu0@gmail.com>
2026-01-15 10:03:44 +0530

    versiohn conflit

--- client/src/components/AddBlogModal.jsx
+++ client/src/components/AddBlogModal.jsx
@@ -7,7 +7,6 @@ const AddBlogModal = ({ isOpen, onClose, onAdd }) => {
         content: '',
         category: 'study-material',
         featuredImage: '',
-
         file: null, // For upload
         tags: ''
     });
